4d8d38027f0b3e4471bb335cdadf702731578f9d
glugeorge <glugeorge@gmail.com>
2024-04-16 17:57:09 -0400

    enforce steady state hydro

--- updated_experiments/S1C_highres.m
+++ updated_experiments/S1C_highres.m
@@ -2,7 +2,7 @@ clear; close all;
 
 %% Define parameter ranges
 A_vals = logspace(log10(3.9e-26),log10(4.2e-25),4);
-M_vals = logspace(-6,-4,4);
+M_vals = logspace(-5,-3,4);
 a_vals = linspace(0.1,0.5,4);
 
 % C values
@@ -58,19 +58,6 @@ params.beta = params.th0/params.t0;
 params.r = params.rho_i/params.rho_w;
 
 params.transient = 0;
-
-%% Grid parameters - ice sheet
-params.Nx = 3000;                    %number of grid points - 200
-params.Nh = 3000;
-fine_grid = linspace(0,1,2*params.Nx);
-params.sigma = fine_grid(2:2:end)';    %grid points on velocity (includes GL, not ice divide)
-params.sigma_elem = fine_grid(1:2:end)';
-params.dsigma = diff(params.sigma_elem); %grid spacing
-
-%% Grid parameters - hydro
-params.sigma_h = linspace(0,1,params.Nh)';
-params.dsigma_h = diff(params.sigma_h); %grid spacing
-
 %% Establish timings
 params.year = 3600*24*365;  %number of seconds in a year
 params.Nt =50;                    %number of time steps - normally 150
@@ -83,8 +70,23 @@ params.dt = params.end_year*params.year/params.Nt;
 params.hydro_u_from_ice_u = 1;
 params.hydro_psi_from_ice_h = 1;
 params.ice_N_from_hydro = 1;
+params.Cf = 1;
+%% Solve and interpolate old
+params.Nx = 300;                    %number of grid points - 200
+params.N1 = 100;                    %number of grid points in coarse domain - 100
+params.Nh = 500;
+params.sigGZ = 0.95;                %extent of coarse grid (where GL is at sigma=1) - 0.97
+sigma1=linspace(params.sigGZ/(params.N1+0.5), params.sigGZ, params.N1);
+sigma2=linspace(params.sigGZ, 1, params.Nx-params.N1+1);
+params.sigma = [sigma1, sigma2(2:end)]';    %grid points on velocity (includes GL, not ice divide)
+params.dsigma = diff(params.sigma); %grid spacing
+params.sigma_elem = [0;(params.sigma(1:params.Nx-1) + params.sigma(2:params.Nx))./2 ]; %grid points on thickness (includes divide, not GL)
 
-%% Initial "steady state" conditions
+% Grid parameters - hydro
+params.sigma_h = linspace(0,1,params.Nh)';
+params.dsigma_h = diff(params.sigma_h); %grid spacing
+
+% Initial "steady state" conditions
 params.shear_scale = 1;
 Q = ones(params.Nh,1);
 N = ones(params.Nh,1);
@@ -93,10 +95,71 @@ params.S_old = S;
 params.M = M./params.M0; % zero when using schoof bed
 params.N_terminus = 0;
 params.accum = accum./params.year;
-xg = 240e3/params.x0;
-hf = (-bed_flat(xg.*params.x0,params)/params.h0)/params.r;
-h =  1 - (1-hf).*params.sigma;
-u = 0.2*(params.sigma_elem.^(1/3)) + 1e-3; % 0.1 for C = 0.5, 0.3 for C = 0.1-0.4
+xg = 100e3/params.x0;
+%hf = (-bed_flat(xg.*params.x0,params)/params.h0)/params.r;
+h =  ones(length(params.sigma_elem),1);%1 - (1-hf).*params.sigma;
+u = 0.1*(params.sigma_elem.^(1/3)) + 1e-3; % 0.1 for C = 0.5, 0.3 for C = 0.1-0.4
+params.Q_in = 0.001/params.Q0;
+
+params.h_old = h;
+params.xg_old = xg;
+params.ice_start = 3*params.Nh;
+
+sig_old = params.sigma;
+sige_old = params.sigma_elem;
+QNShuxg0 = [Q;N;S;h;u;xg];
+bed_func = @bed_flat;
+
+options = optimoptions('fsolve','Display','iter','SpecifyObjectiveGradient',false,'MaxFunctionEvaluations',1e6,'MaxIterations',1e3);
+flf = @(QNShuxg) ice_hydro_equations_coulomb(QNShuxg,params,bed_func);
+
+[QNShuxg_init,F,exitflag,output,JAC] = fsolve(flf,QNShuxg0,options);
+
+Q_old = QNShuxg_init(1:params.Nh);
+N_old = QNShuxg_init(params.Nh+1:2*params.Nh);
+S_old = QNShuxg_init(2*params.Nh+1:3*params.Nh);
+h_old = QNShuxg_init(params.ice_start+1:params.ice_start+ params.Nx);
+u_old = QNShuxg_init(params.ice_start + params.Nx+1:params.ice_start+2*params.Nx);
+xg_old = QNShuxg_init(params.ice_start+2*params.Nx+1);
+sigma_old = params.sigma;
+sigma_elem_old = params.sigma_elem;
+sigma_h_old = params.sigma_h;
+
+%% new
+% Grid parameters - ice sheet
+params.Nx = 3000;                    %number of grid points - 200
+params.Nh = 3000;
+fine_grid = linspace(0,1,2*params.Nx);
+params.sigma = fine_grid(2:2:end)';    %grid points on velocity (includes GL, not ice divide)
+params.sigma_elem = fine_grid(1:2:end)';
+params.dsigma = diff(params.sigma_elem); %grid spacing
+
+%% Grid parameters - hydro
+params.sigma_h = linspace(0,1,params.Nh)';
+params.dsigma_h = diff(params.sigma_h); %grid spacing
+
+
+%% Initial "steady state" conditions
+params.shear_scale = 1;
+%Q = ones(params.Nh,1);
+%N = ones(params.Nh,1);
+%S = ones(params.Nh,1); 
+Q = interp1(sigma_h_old,Q_old,params.sigma_h,"linear","extrap");
+N = interp1(sigma_h_old,N_old,params.sigma_h,"linear","extrap");
+S = interp1(sigma_h_old,S_old,params.sigma_h,"linear","extrap");
+
+params.S_old = S;
+params.M = M./params.M0; % zero when using schoof bed
+params.N_terminus = 0;
+params.accum = accum./params.year;
+%xg = 250e3/params.x0;
+xg = xg_old;
+%hf = (-bed_flat(xg.*params.x0,params)/params.h0)/params.r;
+%h =  ones(length(params.sigma_elem),1);%1 - (1-hf).*params.sigma;
+h = interp1(sigma_elem_old,h_old,params.sigma_elem,"linear","extrap");
+%u = 0.3*(params.sigma_elem.^(1/3)) + 1e-3; % 0.1 for C = 0.5, 0.3 for C = 0.1-0.4
+u = interp1(sigma_old,u_old,params.sigma,"linear","extrap");
+
 params.Q_in = 0.001/params.Q0;
 
 params.h_old = h;
@@ -137,6 +200,9 @@ plot(params.sigma_elem.*xg.*params.x0./1000,h-b_h);ylabel('h');hold on;plot(para
 ax5 = subplot(5,1,5);
 plot(params.sigma.*xg.*params.x0./1000,u);hold on;ylabel('u');xlabel('distance from divide, \emph{x} (km)','Interpreter','latex')
 linkaxes([ax1,ax2,ax3,ax4,ax5],'x')
+%% Save
+save("S1C_highres.mat");
+
 
 function F = combined_hydro_ice_eqns_highres(QNShuxg,params)
     % only for steady state solutions
@@ -224,7 +290,7 @@ function F = combined_hydro_ice_eqns_highres(QNShuxg,params)
     % S
     fs(1) = abs(Q(1)).^3./(S(1).^(8/3)) - ... 
                         S(1).*N(1).^3 + ...
-                        (ss.*params.sigma_h(1)*(xg-xg_old)/dth - params.beta.*u_ice_interp(1)).*(S(2)-S(1))./(xg*params.dsigma_h(1)) - ...
+                        (ss.*params.sigma_h(1)*(xg-xg_old)/dth - params.beta.*u_ice_interp(1)).*0 - ...
                         ss.*(S(1)-params.S_old(1))./dth; % one sided difference
     fs(2:Nh-1)= abs(Q(2:Nh-1)).^3./(S(2:Nh-1).^(8/3)) - ... 
                         S(2:Nh-1).*N(2:Nh-1).^3 + ...
@@ -269,3 +335,121 @@ function F = combined_hydro_ice_eqns_highres(QNShuxg,params)
 
     F = [fq';fn';fs';Fh;Fu;Fxg];
 end
+%% helper functions
+function plot_terms(QNShuxg,params)
+    sigma_elem = params.sigma_elem;
+    sigma_h = params.sigma_h;
+    sigma = params.sigma; 
+%     Q = QNShuxg(1:params.Nx);
+%     N = QNShuxg(params.Nx+1:2*params.Nx);
+%     S = QNShuxg(2*params.Nx+1:3*params.Nx);
+%     h = QNShuxg(3*params.Nx+1:4*params.Nx);
+%     u = QNShuxg(4*params.Nx+1:5*params.Nx);
+%     xg = QNShuxg(5*params.Nx+1);
+    Q = QNShuxg(1:params.Nh);
+    N = QNShuxg(params.Nh+1:2*params.Nh);
+    S = QNShuxg(2*params.Nh+1:3*params.Nh);
+    h = QNShuxg(params.ice_start+1:params.ice_start+ params.Nx);
+    u = QNShuxg(params.ice_start + params.Nx+1:params.ice_start+2*params.Nx);
+    xg = QNShuxg(params.ice_start+2*params.Nx+1);
+    
+    % do ice plots on sigma_elem grid
+    x_grid_ice = sigma*xg*params.x0;
+    diff_ice = diff(x_grid_ice);
+    figure();
+    %% Plot 1 - ice mass conservation
+    % Remember that since these are all in steady state, d/dt goes to zero
+    accum = params.accum; % normalization done in flowline equations
+    h_interp = interp1(sigma_elem,h.*params.h0,sigma,"linear","extrap");
+    dhu_dx = gradient((h_interp.*u).*params.u0)./diff_ice(1);
+    subplot(3,2,1);
+    plot(x_grid_ice./1000,accum.*ones(size(x_grid_ice)),'DisplayName', 'Accumulation');
+    hold on;
+    plot(x_grid_ice./1000,dhu_dx,'--','DisplayName', '$\frac{\partial (hu)}{\partial x}$');
+    legend('Interpreter','latex');
+    xlabel('distance from divide, \emph{x} (km)','Interpreter','latex')
+    title('$\frac{\partial h_g}{\partial t} + \frac{\partial (h_gu)}{\partial x}= a$','Interpreter','latex')
+    %% Plot 2: ice momentum conservation
+    N_interp = interp1(sigma_h,N,sigma,"linear","extrap");
+    shear_stress = params.shear_scale.*params.C.*N_interp(2:end)*params.N0.*(u(2:end).*params.u0./(u(2:end).*params.u0+params.As*(params.C*N_interp(2:end)*params.N0).^params.n)).^(1/params.n); 
+    b = -bed(x_grid_ice,params);
+    driving_stress = params.rho_i*params.g*h_interp(2:end).*gradient(h_interp(2:end)-b(2:end))./gradient(x_grid_ice(2:end));
+    shear_and_driving = shear_stress + driving_stress;
+    
+    dudx = gradient(u(2:end).*params.u0)./gradient(x_grid_ice(2:end));
+    long_stress = gradient(2*params.A^(-1/params.n)*h_interp(2:end).*abs(dudx).^(1/params.n -1).*dudx)./gradient(x_grid_ice(2:end));
+    subplot(3,2,2);
+    plot(x_grid_ice(2:end)./1000,shear_stress,'DisplayName', 'Shear stress');
+    hold on;
+    plot(x_grid_ice(2:end)./1000,driving_stress,'DisplayName', 'Driving stress');
+    plot(x_grid_ice(2:end)./1000,long_stress,'DisplayName', 'Longitudinal stress');
+    plot(x_grid_ice(2:end)./1000,shear_and_driving,'DisplayName', 'Shear + driving stress');
+    legend;
+    xlabel('distance from divide, \emph{x} (km)','Interpreter','latex')
+    title('$\frac{\partial }{\partial x}\left[ 2\bar{A}^{-1/n}h_g\left|\frac{\partial u}{\partial x}\right|^{1/n-1}\frac{\partial u}{\partial x}\right] - CN(\frac{u}{u+A_sC^nN^n})^{1/n} -\rho_igh_g\frac{\partial (h_g-b)}{\partial x}=0$','Interpreter','latex')
+
+    
+    %% Plot 3: Channel Cross sectional area
+    x_grid_hydro = sigma_h*xg*params.x0;
+    m_over_rho_i = params.m0.*abs(Q).^3./(params.rho_i.*S.^(8/3)); % from m'=|Q'|^3/S'^8/3
+    KSN3 = params.K0.*S.*params.S0.*(N.*params.N0).^3;
+    u_interp = interp1(sigma,u.*params.u0,sigma_h,"linear","extrap");
+    u_advect = u_interp.*gradient(S.*params.S0)./gradient(x_grid_hydro);
+    
+    subplot(3,2,3);
+    plot(x_grid_hydro./1000,m_over_rho_i,'k','DisplayName', 'Melt term');
+    hold on;
+    plot(x_grid_hydro./1000,KSN3,'--','DisplayName', '$K_0SN^3$');
+    plot(x_grid_hydro./1000,u_advect,':','DisplayName', 'Advection term');
+    plot(x_grid_hydro./1000,KSN3 + u_advect,'x','DisplayName', '$K_0SN^3$ + Advection term');
+    legend('Interpreter','latex');
+    xlabel('distance from divide, \emph{x} (km)','Interpreter','latex')
+    title('$\frac{\partial S}{\partial t_h} = \frac{m}{\rho_i}-K_0SN^3 - u\frac{\partial S}{\partial x}$',Interpreter='latex')
+    %% Plot 4: Conservation of mass
+    dQdx = gradient(Q.*params.Q0)./gradient(x_grid_hydro);
+    m_over_rho_w = m_over_rho_i.*params.rho_i./params.rho_w;
+    M = ones(size(x_grid_hydro)).*params.M*params.M0;
+    subplot(3,2,4);
+    plot(x_grid_hydro./1000,dQdx,'DisplayName', '$\frac{\partial Q}{\partial x}$');
+    hold on;
+    plot(x_grid_hydro./1000,m_over_rho_w,'DisplayName', 'Melt term');
+    plot(x_grid_hydro./1000,M,'DisplayName', 'Supply term');
+    legend('Interpreter','latex');
+    xlabel('distance from divide, \emph{x} (km)','Interpreter','latex')
+    title('$\frac{\partial S}{\partial t_h} + \frac{\partial Q}{\partial x} = \frac{m}{\rho_w}+M$','Interpreter','latex')
+    %% Plot 5: Momentum conservation
+    dNdx = gradient(N.*params.N0)./gradient(x_grid_hydro);
+    f_Q_S = params.f*params.rho_w*params.g.*Q.*params.Q0.*abs(Q.*params.Q0)./(S.*params.S0).^(8/3);
+    h_interp = interp1(sigma_elem,h,sigma_h,'linear','extrap');
+    b = -bed(x_grid_hydro,params);
+    psi = params.rho_w*params.g*gradient(b)./gradient(x_grid_hydro) - gradient(params.rho_i*params.g*h_interp*params.h0)./gradient(x_grid_hydro);
+    subplot(3,2,5);
+    plot(x_grid_hydro./1000,psi,'DisplayName', '$\psi$');
+    hold on;
+    plot(x_grid_hydro./1000,dNdx,'DisplayName', '$\frac{\partial N}{\partial x}$');
+    plot(x_grid_hydro./1000,f_Q_S,'DisplayName', '$f\rho_wg\frac{Q|Q|}{S^{8/3}}$');
+    plot(x_grid_hydro./1000,psi+dNdx,'--','DisplayName', '$\psi+\frac{\partial N}{\partial x}$');
+    
+    legend('Interpreter','latex');
+    xlabel('distance from divide, \emph{x} (km)','Interpreter','latex')
+    title('$\psi+ \frac{\partial N}{\partial x} = f\rho_wg\frac{Q|Q|}{S^{8/3}}$','Interpreter','latex')
+    %% Plot 6: Energy
+    melt_term = m_over_rho_i.*params.rho_i*params.L;
+    flow_through_potential = Q.*params.Q0 .*(psi + dNdx);
+    subplot(3,2,6);
+    plot(x_grid_hydro./1000,melt_term,'DisplayName', 'Melt');
+    hold on;
+    plot(x_grid_hydro./1000,flow_through_potential,'DisplayName', 'Flow through potential');
+    legend('Interpreter','latex');
+    xlabel('distance from divide, \emph{x} (km)','Interpreter','latex')
+    title('$mL = Q(\psi + \frac{\partial N}{\partial x})$','Interpreter','latex')
+
+    figure()
+    plot(x_grid_hydro./1000,params.rho_w*params.g*gradient(b)./gradient(x_grid_hydro),'DisplayName','$\rho_wg\frac{\partial b}{\partial x}$');
+    hold on; 
+    plot(x_grid_hydro./1000,gradient(params.rho_i*params.g*h_interp*params.h0)./gradient(x_grid_hydro),'DisplayName','$\rho_ig\frac{\partial h}{\partial x}$')
+    plot(x_grid_hydro./1000,psi,'DisplayName', '$\psi$');    
+    legend('Interpreter','latex');
+
+
+end 
